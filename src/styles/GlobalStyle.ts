afa59f1a968ee2eb66c960e200c5b22266922c71
Lucas Cunha <lucasviana112@gmail.com>
2020-12-01 23:36:50 -0300

    adjustment the global style them

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,14 +1,14 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-    body {
-        background: #121214;
-        color: #eee;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-`
+  body {
+    background: #fff;
+    color: #121214;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
