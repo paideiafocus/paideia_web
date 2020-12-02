90a23c736e2e25ab364568402cbce375e162e244
Lucas Cunha <lucasviana112@gmail.com>
2020-12-01 23:39:25 -0300

    created the Banner component for Home page

---
+++ src/content/Home/Banner/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+import * as S from './styles';
+
+const Banner: React.FC = () => {
+  return (
+    <S.BannerImage>
+      <Image
+        src="/image_01.png"
+        alt="Curso pré-vestibular gratuito"
+        height="auto"
+        width="900"
+        // layout="responsive"
+        priority
+      />
+    </S.BannerImage>
+  );
+};
+
+export default Banner;
---
+++ src/content/Home/Banner/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const BannerImage = styled.div`
+  background: red;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 24rem;
+`;
