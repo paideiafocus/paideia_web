7b9cd460e60d0e3abdc207e6b627c0b2c8145d72
Lucas Cunha <lucasviana112@gmail.com>
2020-12-01 23:39:42 -0300

    created the Depositions component for Home page

---
+++ src/content/Home/Depositions/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+
+import * as S from './styles';
+
+const Depositions: React.FC = () => {
+  return (
+    <S.DepositionsContainer>
+      <h2>depoimentos</h2>
+    </S.DepositionsContainer>
+  );
+};
+
+export default Depositions;
