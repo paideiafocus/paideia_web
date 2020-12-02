868455c2e59b18dafd5c694623d097c87601356a
Lucas Cunha <lucasviana112@gmail.com>
2020-12-02 00:04:06 -0300

    adjustment the structure of the Depositions component

--- src/content/Home/Depositions/index.tsx
+++ src/content/Home/Depositions/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 
 import * as S from './styles';
 
@@ -7,6 +6,30 @@ const Depositions: React.FC = () => {
   return (
     <S.DepositionsContainer>
       <h2>depoimentos</h2>
+      <S.DepositionsContent>
+        <S.DepositionsCard borderDirection="right">
+          <p>
+            Não há palavras suficientes para descrever a imensa gratidão que
+            tenho pelo Cursinho Focus e por cada um dos professores que nos
+            ensinou, e tenho certeza que continuam ensinando, com muita
+            sabedoria...
+          </p>
+          <strong>Patricia Evelin</strong>
+        </S.DepositionsCard>
+
+        <S.DepositionsCard borderDirection="left">
+          <p>
+            Não há palavras suficientes para descrever a imensa gratidão que
+            tenho pelo Cursinho Focus e por cada um dos professores que nos
+            ensinou, e tenho certeza que continuam ensinando, com muita
+            sabedoria...
+          </p>
+          <strong>Patricia Evelin</strong>
+        </S.DepositionsCard>
+      </S.DepositionsContent>
+      <S.SeeMoreDepositions>
+        <button type="button">Ver mais depoimentos</button>
+      </S.SeeMoreDepositions>
     </S.DepositionsContainer>
   );
 };
--- src/content/Home/Depositions/styles.ts
+++ src/content/Home/Depositions/styles.ts
@@ -1,3 +1,43 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const DepositionsContainer = styled.section``;
+
+export const DepositionsContent = styled.div`
+  display: flex;
+`;
+
+interface IDepositionsCardProps {
+  borderDirection: 'right' | 'left';
+}
+
+export const DepositionsCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 2rem;
+
+  ${(p: IDepositionsCardProps) =>
+    p.borderDirection === 'right' &&
+    css`
+      border-right: 1px solid;
+    `}
+
+  ${(p: IDepositionsCardProps) =>
+    p.borderDirection === 'left' &&
+    css`
+      border-left: 1px solid;
+    `}
+
+  strong {
+    text-align: end;
+  }
+`;
+
+export const SeeMoreDepositions = styled.div`
+  margin: 2.2rem 0;
+  text-align: center;
+
+  button {
+    padding: 0.7rem;
+    cursor: pointer;
+  }
+`;
