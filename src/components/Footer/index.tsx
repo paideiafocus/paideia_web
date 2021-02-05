d1a0e955368715e6677fd1e8b750fa4463075b9c
Lucas Cunha <lucasviana112@gmail.com>
2021-02-04 23:49:31 -0300

    adjustment the Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-
-// import * as S from './styles';
+import Image from 'next/image';
+import Grid from '@material-ui/core/Grid';
+import * as S from './styles';
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <div>footer</div>
-    </footer>
+    <S.Footer>
+      <Grid container alignItems="center" spacing={3}>
+        <Grid item xs={11}>
+          Copyright © Associação Paideia - Todos os direitos reservados
+        </Grid>
+        <Grid item xs={1}>
+          <a href="https://www.facebook.com/focuscursinho/" target="_blank">
+            <Image src="/face_light.png" width="38" height="38" />
+          </a>
+        </Grid>
+      </Grid>
+    </S.Footer>
   );
 };
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,15 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import theme from '@/config/theme';
 
-// export const PageContainer = styled.section``;
-// export const PageContent = styled.div``;
+const { color } = theme;
+
+export const Footer = styled.footer`
+  background-color: ${color.green.normal};
+  padding: 1rem 1.5rem;
+  color: #fff;
+  border: 1px solid #dedede;
+  border-radius: 0.2rem;
+  box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.055);
+  overflow: hidden;
+  bottom: 0;
+`;
