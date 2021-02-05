7fe1a93d2ea4280ded46d7791687110dede7c658
Lucas Cunha <lucasviana112@gmail.com>
2021-02-04 23:50:28 -0300

    configuration color theme

---
+++ src/config/theme/color.ts
@@ -0,0 +1,22 @@
+const color = {
+  pink: {
+    dark: '#c757a1',
+    normal: '#d86ab2',
+    light: '#dd9bc7',
+  },
+  green: {
+    dark: '#527a25',
+    normal: '#86c240',
+    light: '#bff97c',
+  },
+  gray: {
+    darker: 'rgb(95, 94, 94)',
+    dark: '#6e6e6e',
+    normal: '#cccccc',
+    light: '#dedede',
+    lighter: '#E0E0E0',
+  },
+};
+
+export default color;
+// #eceff1
