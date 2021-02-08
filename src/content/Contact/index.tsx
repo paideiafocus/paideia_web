9e5407e4b2fd7e040bf9dfe1e33f2312127ca9db
Lucas Cunha <lucasviana112@gmail.com>
2021-02-07 21:18:00 -0300

    migrate infos for Contact page

--- src/content/Contact/index.tsx
+++ src/content/Contact/index.tsx
@@ -1,10 +1,22 @@
 import { memo } from 'react';
 
+import Page from '@/components/Page';
+import * as S from './styles';
+
 const Contact = () => {
   return (
-    <div>
-      <h4>Contact</h4>
-    </div>
+    <Page align="center">
+      <S.ContactSection>
+        <h1>CONTATO</h1>
+
+        <p>
+          Para entrar em contato envie uma mensagem através do Facebook ou envie
+          um email para:
+          <br />
+          contato@associacaopaideia.org.br
+        </p>
+      </S.ContactSection>
+    </Page>
   );
 };
 
