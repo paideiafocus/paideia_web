355761b85b719ffd655539e3fdddfd69ef76e9f5
Lucas Cunha <lucasviana112@gmail.com>
2021-04-21 23:58:48 -0300

    adjustment spaces and positions

--- src/content/About/index.tsx
+++ src/content/About/index.tsx
@@ -12,7 +12,8 @@ const About = () => {
           <Grid item xs={12}>
             <h1> SOBRE </h1>
             <h2> Um Projeto Social - Um pouco das nossas origens </h2>
-            <p>
+
+            <S.TextBlock>
               <b>
                 <i>
                   “Olhar não é apenas dirigir os olhos para perceber o real fora
@@ -22,14 +23,16 @@ const About = () => {
                   um projeto.”
                 </i>
               </b>
-            </p>
-            <p>
+            </S.TextBlock>
+
+            <S.TextBlock>
               Foi a partir deste olhar que iniciamos, no ano de 2003, as
               discussões para a organização de uma associação civil sem fins
               lucrativos e econômicos na cidade de Louveira, a Associação
               Paidéia.
-            </p>
-            <p>
+            </S.TextBlock>
+
+            <S.TextBlock>
               No Salão Paroquial da Igreja de São Sebastião nos reunimos todos,
               três professores e mais dezesseis jovens, que ansiosos e vibrantes
               com a idéia, ajudaram a construir as bases do que seria o Focus
@@ -37,21 +40,23 @@ const About = () => {
               de alunos egressos do sistema público de ensino que, por conta de
               suas condições materiais, não teriam condições de arcar com os
               custos de um curso pré-vestibular particular.
-            </p>
-            <p>
+            </S.TextBlock>
+
+            <S.TextBlock>
               Foram precisos mais dois anos de caminhada para que o sonho se
               concretizasse. E assim, em agosto de 2005, em parceria com a
               Prefeitura Municipal de Louveira, iniciamos nossas atividades em
               uma das salas da E. M. E. F. Vila Pasti.
-            </p>
-            <p>
+            </S.TextBlock>
+
+            <S.TextBlock>
               O cursinho comunitário Focus Vestibulares da Associação Paidéia é
               um projeto social sem fins lucrativos, mantido pela Prefeitura
               Municipal de Louveira e cujo objetivo é possibilitar o acesso
               gratuito a um curso pré-vestibular de qualidade dando aos jovens
               que não podem arcar com os custos de um cursinho particular a
               possibilidade de prosseguirem seus estudos em nível superior.
-            </p>
+            </S.TextBlock>
           </Grid>
 
           <br />
@@ -62,74 +67,91 @@ const About = () => {
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Preparatório ENEM</h6>
-            <p>
-              Além da preparação para o vestibular os nossos alunos contam ainda
-              com um preparatório para o ENEM com aulas, material e testes
-              específicos para este processo seletivo.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Preparatório ENEM</h6>
+              <p>
+                Além da preparação para o vestibular os nossos alunos contam
+                ainda com um preparatório para o ENEM com aulas, material e
+                testes específicos para este processo seletivo.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Aulas Complementares Específicas</h6>
-            <p>
-              Nestas aulas os alunos recebem orientação específica sobre cada
-              conteúdo ministrado além de participarem da resolução de listas de
-              exercícios.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Aulas Complementares Específicas</h6>
+              <p>
+                Nestas aulas os alunos recebem orientação específica sobre cada
+                conteúdo ministrado além de participarem da resolução de listas
+                de exercícios.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Material Didático</h6>
-            <p>
-              Ético Sistema de Ensino, da Editora Saraiva tem o conteúdo exigido
-              pelos principais vestibulares do Brasil e tudo isso atualizado,
-              colorido e conforme o novo acordo ortográfico.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Material Didático</h6>
+              <p>
+                Ético Sistema de Ensino, da Editora Saraiva tem o conteúdo
+                exigido pelos principais vestibulares do Brasil e tudo isso
+                atualizado, colorido e conforme o novo acordo ortográfico.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Simulados</h6>
-            <p>
-              Com questões dos principais vestibulares para que os alunos possam
-              avaliar seu aproveitamento.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Simulados</h6>
+              <p>
+                Com questões dos principais vestibulares para que os alunos
+                possam avaliar seu aproveitamento.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Oficinas de Redação</h6>
-            <p>
-              Necessárias para o aprimoramento tanto nos vestibulares como para
-              o ENEM.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Oficinas de Redação</h6>
+              <p>
+                Necessárias para o aprimoramento tanto nos vestibulares como
+                para o ENEM.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Acompanhamento Psicológico</h6>
-            <p>
-              Atendimento especializado visando ajudar na escolha profissional.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Acompanhamento Psicológico</h6>
+              <p>
+                Atendimento especializado visando ajudar na escolha
+                profissional.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Roteiros Culturais</h6>
-            <p>
-              Visitas a Centros Culturais, Teatros, Museus, Universidades entre
-              outros.
-            </p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Roteiros Culturais</h6>
+              <p>
+                Visitas a Centros Culturais, Teatros, Museus, Universidades
+                entre outros.
+              </p>
+            </S.ServiceColumn>
           </Grid>
 
           <Grid item xs={3}>
-            <div className="separa" />
-            <h6>Cursinho Online</h6>
-            <p>Plantão de dúvidas virtual (em implantação).</p>
+            <S.ServiceColumn>
+              <div className="separa" />
+              <h6>Cursinho Online</h6>
+              <p>Plantão de dúvidas virtual (em implantação).</p>
+            </S.ServiceColumn>
           </Grid>
         </Grid>
       </S.AboutContainer>
--- src/content/About/styles.ts
+++ src/content/About/styles.ts
@@ -4,19 +4,40 @@ import theme from '@/config/theme';
 const { color } = theme;
 
 export const AboutContainer = styled.section`
+  margin-bottom: 2rem;
+
   p {
     text-justify: auto;
+    text-align: justify;
   }
+
   .separa {
     border-top: 4px ${color.green.normal} solid;
     margin-bottom: 13px;
     margin-top: 5px;
   }
+
   h6 {
     color: ${color.pink.dark};
     text-align: center;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
   }
+
   h2 {
     text-align: center;
+  }
+`;
+
+export const TextBlock = styled.p`
+  margin-bottom: 1rem;
+`;
+
+export const ServiceColumn = styled.div`
+  padding: 0 0.5rem;
+
+  p {
+    text-align: left;
   }
 `;
