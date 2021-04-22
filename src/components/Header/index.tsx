0c331d8708149252a09eaf0325c00f49d5e78388
Lucas Cunha <lucasviana112@gmail.com>
2021-04-21 23:57:34 -0300

    initial structure menu desktop and mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
 import { memo, useState, useCallback } from 'react';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 import * as S from './styles';
 
 const navLinks = [
@@ -13,9 +23,60 @@ const navLinks = [
   { id: 5, path: '/acesso' },
 ];
 
+// const useStyles = makeStyles({
+//   list: {
+//     width: 250,
+//   },
+//   fullList: {
+//     width: 'auto',
+//   },
+// });
+
 const Header = () => {
   const router = useRouter();
   const [navId, setNavId] = useState(0);
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+  const toggleDrawer = open => event => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setMobileMenuIsOpen(() => open);
+  };
+
+  const list = () => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   const handleNavigation = useCallback(
     (event, newNavId) => {
@@ -32,21 +93,32 @@ const Header = () => {
       <S.ImageContainer>
         <img src="/logo/logo_paideia.png" alt="Logo Paideia" />
       </S.ImageContainer>
-      {/* <Image
-          src="/logo/logo_paideia.png"
-          width="1496"
-          height="314"
-          layout="responsive"
-          alt="Logo Paideia"
-        /> */}
-      <BottomNavigation value={navId} onChange={handleNavigation} showLabels>
-        <BottomNavigationAction label="Início" />
-        <BottomNavigationAction label="Sobre" />
-        <BottomNavigationAction label="Notícias" />
-        <BottomNavigationAction label="Contato" />
-        <BottomNavigationAction label="Perguntas Frequentes" />
-        <BottomNavigationAction label="Acesse sua conta" />
-      </BottomNavigation>
+
+      <S.DesktopMenu>
+        <BottomNavigation value={navId} onChange={handleNavigation} showLabels>
+          <BottomNavigationAction label="Início" />
+          <BottomNavigationAction label="Sobre" />
+          <BottomNavigationAction label="Notícias" />
+          <BottomNavigationAction label="Contato" />
+          <BottomNavigationAction label="Perguntas Frequentes" />
+          <BottomNavigationAction label="Acesse sua conta" />
+        </BottomNavigation>
+      </S.DesktopMenu>
+
+      <S.MobileMenu>
+        <div>
+          <Button onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </Button>
+          <Drawer
+            anchor="left"
+            open={mobileMenuIsOpen}
+            onClose={toggleDrawer(false)}
+          >
+            {list()}
+          </Drawer>
+        </div>
+      </S.MobileMenu>
     </nav>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,3 +9,19 @@ export const ImageContainer = styled.div`
     width: 86%;
   }
 `;
+
+export const DesktopMenu = styled.div`
+  display: none;
+
+  @media (min-width: 46rem) {
+    display: block;
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: block;
+
+  @media (min-width: 46rem) {
+    display: none;
+  }
+`;
