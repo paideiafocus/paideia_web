928ef5277828a0622657c1a89d12bf8a661ab2cd
Lucas Cunha <lucasviana112@gmail.com>
2021-05-07 22:41:06 -0300

    create the page for register new user

---
+++ src/content/Register/index.tsx
@@ -0,0 +1,106 @@
+import { memo } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Alert from '@/components/Alert';
+import Page from '@/components/Page';
+
+import * as S from './styles';
+
+const Register = () => {
+  return (
+    <Page>
+      <div>
+        <h1>CADASTRO AO SISTEMA</h1>
+
+        <div>
+          <div style={{ textAlign: 'center' }}>
+            <p>
+              O cadastro ao sistema deve ser realizado para prosseguir com a
+              inscrição.
+            </p>
+
+            <p>
+              Após realizar o cadastro, você receberá um e-mail com o código de
+              confirmação, e após validar o código no site, poderá acessar sua
+              conta para fazer a inscrição.
+            </p>
+
+            <br />
+            <b>ATENÇÃO! Esse cadastro não garante sua vaga ao projeto!</b>
+            <br />
+            <br />
+          </div>
+
+          <div style={{ textAlign: 'center' }}>
+            <Alert kind="danger">
+              Cadastro válido somente durante o periodo de inscrição!
+            </Alert>
+          </div>
+
+          <S.Form>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <S.TextFieldCustom
+                  id="name"
+                  name="name"
+                  label="Digite seu nome"
+                  variant="outlined"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <S.TextFieldCustom
+                  id="lastname"
+                  name="lastname"
+                  label="Sobrenome (completo)"
+                  variant="outlined"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <S.TextFieldCustom
+                  id="email"
+                  name="email"
+                  label="Digite seu e-mail"
+                  variant="outlined"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <S.TextFieldCustom
+                  id="password"
+                  name="password"
+                  label="Senha"
+                  variant="outlined"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <S.TextFieldCustom
+                  id="confirm_password"
+                  name="confirm_password"
+                  label="Confirme sua senha"
+                  variant="outlined"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Button variant="contained" color="primary">
+                  Cadastrar
+                </Button>
+              </Grid>
+            </Grid>
+          </S.Form>
+        </div>
+      </div>
+    </Page>
+  );
+};
+
+export default memo(Register);
---
+++ src/content/Register/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+
+export const Form = styled.form`
+  margin-bottom: 2rem;
+`;
+
+export const TextFieldCustom = styled(TextField)`
+  width: 100%;
+`;
