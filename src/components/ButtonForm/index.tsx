f2910170bfc3f8a7a77a427c802cdeb551922c32
Lucas Cunha <lucasviana112@gmail.com>
2021-05-15 16:19:08 -0300

    create ButtonForm component

---
+++ src/components/ButtonForm/index.tsx
@@ -0,0 +1,34 @@
+import { memo, MouseEvent, ReactNode } from 'react';
+import ButtonMaterial from '@material-ui/core/Button';
+import { CircularProgress } from '@material-ui/core';
+import * as S from './styles';
+
+interface IButtonFormProps {
+  disabled?: boolean;
+  onClick(e: MouseEvent<HTMLElement>): void;
+  loading?: boolean;
+  children: ReactNode;
+}
+
+const ButtonForm: React.FC<IButtonFormProps> = ({
+  disabled,
+  onClick,
+  loading,
+  children,
+}) => {
+  return (
+    <ButtonMaterial
+      variant="contained"
+      color="primary"
+      disabled={disabled || false}
+      onClick={onClick}
+    >
+      <S.Content>
+        {!loading && children}
+        {loading && <CircularProgress color="secondary" />}
+      </S.Content>
+    </ButtonMaterial>
+  );
+};
+
+export default memo(ButtonForm);
---
+++ src/components/ButtonForm/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  min-width: 10rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 1.7rem !important;
+    height: 1.7rem !important;
+  }
+`;
