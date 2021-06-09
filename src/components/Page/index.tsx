939a724051e2622534d7fb679f4b3136c298d6a5
Lucas Cunha <lucasviana112@gmail.com>
2021-06-09 11:54:52 -0300

    configuration SEO

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Footer from '../Footer';
 import Header from '../Header';
 
@@ -11,6 +12,32 @@ interface PageProps {
 const Page: React.FC<PageProps> = ({ align, isFull, children }) => {
   return (
     <S.PageContainer>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Associação Paideia</title>
+        <meta
+          name="description"
+          content="A Associação Paideia é uma ONG destinada a preparar jovens para os vestibulares."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="copyright" content="Associação Paideia" />
+        <meta
+          name="abstract"
+          content="A Associação Paideia é uma ONG destinada a preparar jovens para os vestibulares."
+        />
+        <meta
+          property="og:url"
+          content="https://www.associacaopaideia.org.br/inicio"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt-BR" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/assets/logo/square-logo.png"
+        />
+      </Head>
+
       <Header />
       <S.PageContent align={align} isFull={isFull}>
         {children}
