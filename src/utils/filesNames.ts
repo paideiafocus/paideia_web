40f6cef36bad9fbbd2ad966252a537f0cec6b351
Lucas Cunha <lucasviana112@gmail.com>
2021-06-11 13:09:00 -0300

    translate objects

---
+++ src/utils/filesNames.ts
@@ -0,0 +1,12 @@
+const filesNames = {
+  CITIZEN: 'Cartão Cidadão',
+  SCHOLARSHIP: 'Bolsa',
+  PHOTO: 'Foto',
+  CPF: 'CPF',
+  HISTORIC: 'Histórico',
+  EJA: 'Termo',
+  ADDRESS: 'Comprovante de endereço',
+  RG: 'RG',
+};
+
+export default filesNames;
---
+++ src/utils/statusFormat.ts
@@ -0,0 +1,10 @@
+const statusFormat = {
+  admin: 'administrador',
+  common: 'comum',
+  active: 'ativo',
+  subscriber: 'lista regular',
+  waiting: 'lista de espera',
+  canceled: 'inscrição cancelada',
+};
+
+export default statusFormat;
