79c6af944ad4bf44d9fdffd9868836748c879cd3
Lucas Cunha <lucasviana112@gmail.com>
2021-06-12 01:13:12 -0300

    configure babel + styled component + material UI for ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,49 @@
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
 import GlobalStyle from '../styles/GlobalStyle';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
 
-export default MyApp;
+// import GlobalStyle from '../styles/GlobalStyle';
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <>
+//       <GlobalStyle />
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
+// export default MyApp;
