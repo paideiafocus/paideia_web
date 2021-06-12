5328406daeede50ae26fb2ad531f0e89371ed11d
Lucas Cunha <lucasviana112@gmail.com>
2021-06-12 01:32:35 -0300

    adjustment SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,10 +19,28 @@ export default function MyApp(props) {
   return (
     <>
       <Head>
-        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Associação Paideia</title>
         <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          name="description"
+          content="A Associação Paideia é uma ONG destinada a preparar jovens para os vestibulares."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="copyright" content="Associação Paideia" />
+        <meta
+          name="abstract"
+          content="A Associação Paideia é uma ONG destinada a preparar jovens para os vestibulares."
+        />
+        <meta
+          property="og:url"
+          content="https://www.associacaopaideia.org.br/inicio"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt-BR" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/assets/logo/square-logo.png"
         />
       </Head>
       <ThemeProvider theme={theme}>
