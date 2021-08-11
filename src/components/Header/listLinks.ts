23e067c72aed2fcc5a70487545249a3e0a174449
paideiafocus <admin@associacaopaideia.org.br>
2021-08-10 21:10:08 -0300

    inscrição2

--- src/components/Header/listLinks.ts
+++ src/components/Header/listLinks.ts
@@ -20,14 +20,14 @@ export const NotLoggedLinks: INavLinks[] = [
 
 export const linksToActiveUsers: INavLinks[] = [
   ...allNavLinks,
-  // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
   // { id: 6, path: '/sair', label: 'Sair' },
-  { id: 5, path: '/sair', label: 'Sair' },
+  { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 export const linksToSubscriberUsers: INavLinks[] = [
   ...allNavLinks,
-   { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
    { id: 6, path: '/simulado', label: 'Simulado' },
    { id: 7, path: '/sair', label: 'Sair' },
   // { id: 5, path: '/simulado', label: 'Simulado' },
