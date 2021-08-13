a4d9947cd92d759113827f4a19d835de79327483
paideiafocus <admin@associacaopaideia.org.br>
2021-08-13 17:53:11 -0300

    simulado

--- src/pages/simulado.tsx
+++ src/pages/simulado.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from 'react';
 import PrivatePage from '@/components/PrivatePage';
-import Simulado from '../content/simulado';
+import Simulado from '../content/Simulado';
 
 const SimuladoPage: FC = () => (
   <PrivatePage>
