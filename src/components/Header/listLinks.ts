7b7f70d1c94c410b99a65f87a7a06beee2a9912e
paideiafocus <admin@associacaopaideia.org.br>
2021-08-13 18:07:56 -0300

    simulado 4

--- src/components/Header/listLinks.ts
+++ src/components/Header/listLinks.ts
@@ -28,8 +28,8 @@ export const linksToActiveUsers: INavLinks[] = [
 export const linksToSubscriberUsers: INavLinks[] = [
   ...allNavLinks,
   // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
-   { id: 5, path: '/Simulado', label: 'Simulado' },
-   { id: 6, path: '/sair', label: 'Sair' },
+   { id: 6, path: '/simulado', label: 'Simulado' },
+   { id: 7, path: '/sair', label: 'Sair' },
   // { id: 5, path: '/simulado', label: 'Simulado' },
   //{ id: 5, path: '/sair', label: 'Sair' },
 ];
