34180522113e619ef4a0f515899834b4fa781988
paideiafocus <admin@associacaopaideia.org.br>
2022-06-12 19:29:17 -0300

    -GlobalStyle _app.tsx 49:8

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,7 +46,6 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
