db2bb2a4a227a5547a45b7e25a66f4db9919f406
paideiafocus <admin@associacaopaideia.org.br>
2022-06-12 15:16:42 -0300

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,6 +46,7 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
