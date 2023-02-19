a2de638967d3595b278cd4650d9e4f4862d90de4
Lucas Cunha <lucas.cunha@luizalabs.com>
2023-02-19 10:48:16 -0300

    chore: disable nav links

--- src/components/Header/listLinks.ts
+++ src/components/Header/listLinks.ts
@@ -15,31 +15,31 @@ export const allNavLinks: INavLinks[] = [
 export const NotLoggedLinks: INavLinks[] = [
   ...allNavLinks,
   { id: 5, path: '/acesso', label: 'Acesse sua conta' },
-  //{ id: 6, path: '/cadastro', label: 'Cadastre-se' },
+  // { id: 6, path: '/cadastro', label: 'Cadastre-se' },
 ];
 
 export const linksToActiveUsers: INavLinks[] = [
   ...allNavLinks,
-  //{ id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
   { id: 5, path: '/sair', label: 'Sair' },
-  //{ id: 6, path: '/sair', label: 'Sair' },
+  // { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 export const linksToSubscriberUsers: INavLinks[] = [
   ...allNavLinks,
-   //{ id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
   // { id: 5, path: '/simulado', label: 'Simulado' },
-   //{ id: 5, path: '/sair', label: 'Sair' },
+  // { id: 5, path: '/sair', label: 'Sair' },
   // { id: 6, path: '/simulado', label: 'Simulado' },
-   { id: 7, path: '/sair', label: 'Sair' },
+  { id: 7, path: '/sair', label: 'Sair' },
 ];
 
 export const linksToAdminUsers: INavLinks[] = [
   ...allNavLinks,
   { id: 5, path: '/adm/inscritos', label: 'Administrador' },
-  { id: 6, path: '/adm/simulado-resultado-completo', label: 'Resultado Simulado' },
-  { id: 7, path: '/adm/simulado-cadastro', label: 'Cadastro Simulado' },
-  { id: 8, path: '/sair', label: 'Sair' },
+  // { id: 6, path: '/adm/simulado-resultado-completo', label: 'Resultado Simulado' },
+  // { id: 7, path: '/adm/simulado-cadastro', label: 'Cadastro Simulado' },
+  { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 // links for others status: admin, subscriber, waiting, canceled
