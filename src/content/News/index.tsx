91b4a9992be0c331193a7aae2ca1b2cd0e60da0b
Lucas Cunha <lucas.cunha@luizalabs.com>
2023-03-08 00:08:18 -0300

    feat: adjustment the facebook news

--- src/content/News/index.tsx
+++ src/content/News/index.tsx
@@ -26,19 +26,27 @@ const News = () => {
         >
           <span>
             <iframe
-              name="f78291fa5842fc"
+              title="facebook"
+              // src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpg%2Ffocuscursinho&tabs=timeline&width=0&height=0&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffocuscursinho"
               width="1000px"
               height="1000px"
-              data-testid="fb:page Facebook Social Plugin"
-              title="fb:page Facebook Social Plugin"
-              frameBorder="0"
-              // allowtransparency="true"
+              style={{ border: 'none', overflow: 'hidden' }}
               allowFullScreen
-              scrolling="no"
-              allow="encrypted-media"
-              src="https://web.facebook.com/v3.0/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Dfed8b4b1d34bac%26domain%3Dlocalhost%26origin%3Dhttp%253A%252F%252Flocalhost%253A4001%252Ff3476196162465%26relation%3Dparent.parent&amp;container_width=1110&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fpg%2Ffocuscursinho&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline"
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
             />
           </span>
+
+          {/* <span>
+            <iframe
+              id="frame"
+              width="320"
+              height="568"
+              src="https://instagram.com/focuscursinho?igshid=YmMyMTA2M2Y="
+            // src="https://www.instagram.com/p/BxFc58nFwpN/embed"
+            // frameBorder="0"
+            />
+          </span> */}
         </S.FaceContainer>
       </div>
     </Page>
