9a900065b2eb49dd232bc95e24dea255a40265d8
Lucas Cunha <lucas.cunha@luizalabs.com>
2024-04-16 21:51:14 -0300

    feat: open inscription

--- src/components/Header/listLinks.ts
+++ src/components/Header/listLinks.ts
@@ -15,32 +15,32 @@ export const allNavLinks: INavLinks[] = [
 export const NotLoggedLinks: INavLinks[] = [
   ...allNavLinks,
   { id: 5, path: '/acesso', label: 'Acesse sua conta' },
-  // { id: 6, path: '/cadastro', label: 'Cadastre-se' },
+  { id: 6, path: '/cadastro', label: 'Cadastre-se' },
 ];
 
 export const linksToActiveUsers: INavLinks[] = [
   ...allNavLinks,
-  // { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
-  { id: 5, path: '/sair', label: 'Sair' },
+  { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 export const linksToSubscriberUsers: INavLinks[] = [
   ...allNavLinks,
   // { id: 5, path: '/simulado', label: 'Simulado' },
-  // { id: 6, path: '/inscricao/termo', label: 'Quero me inscrever' },
-  { id: 5, path: '/sair', label: 'Sair' },
+  { id: 5, path: '/inscricao/termo', label: 'Quero me inscrever' },
+  { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 export const linksToAdminUsers: INavLinks[] = [
   ...allNavLinks,
   { id: 5, path: '/adm/inscritos', label: 'Administrador' },
-  {
-    id: 6,
-    path: '/adm/simulado-resultado-completo',
-    label: 'Resultado Simulado',
-  },
-  { id: 7, path: '/adm/simulado-cadastro', label: 'Cadastro Simulado' },
-  { id: 8, path: '/sair', label: 'Sair' },
+  // {
+  //   id: 6,
+  //   path: '/adm/simulado-resultado-completo',
+  //   label: 'Resultado Simulado',
+  // },
+  // { id: 7, path: '/adm/simulado-cadastro', label: 'Cadastro Simulado' },
+  { id: 6, path: '/sair', label: 'Sair' },
 ];
 
 // links for others status: admin, subscriber, waiting, canceled
